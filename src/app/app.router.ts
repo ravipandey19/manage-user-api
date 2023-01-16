02b973057457002265fc7dcd155ba47ad323809d
Ravi Kumar Pandey <ravikumar.pandey@ad.infosys.com>
2023-01-16 20:58:24 +0530

    adding api validation

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -20,9 +20,14 @@ _router.get("/getData", async (req: Request, res: Response) => {
 // POST data
 _router.post("/insertData", async (req: Request, res: Response) => {
     try {
+        if (/\d/.test(req.body.first_name) || /\d/.test(req.body.last_name)) return res.status(400).json('Name contains a number.');
+
+        if (!(/(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/.test(req.body.emailid))) return res.status(400).json('Email id is not valid.');
+
         let _d: any = new _db(req.body);
         const r: {} = await _d.save();
         res.status(201).json(r);
+        
     } catch (e: any) {
         res.status(500).send(e.message);
     }
